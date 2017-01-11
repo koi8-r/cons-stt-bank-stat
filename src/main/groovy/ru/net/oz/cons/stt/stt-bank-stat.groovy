c326f1c4522088b344e10a526731f15557d5b238
koi8-r <Burchenya_VP@murmangaz.ru>
2017-01-11 13:56:30 +0300

    Ignore cli args Multiple dirs  Idea project file git ignore  Fix wrong header check

--- src/main/groovy/ru/net/oz/cons/stt/stt-bank-stat.groovy
+++ src/main/groovy/ru/net/oz/cons/stt/stt-bank-stat.groovy
@@ -3,7 +3,7 @@ package ru.net.oz.cons.stt
 /*
     Structure:
     +---------------------------+
-    | S | T | T | ? | ? | 0 | 0 | ; 7 byte header
+    | S | T | T | ? | ? | ? | ? | ; 7 byte header
     +---------------------------+
     |          zlib data        |
     +---------------------------+
@@ -13,9 +13,11 @@ package ru.net.oz.cons.stt
 
     Info banks:
     https://www.consultant.ru/about/software/systems/
+
+    Win cmd unicode:
+    chcp 65001
  */
 
-import java.lang.System as sys
 import java.util.zip.InflaterInputStream
 
 def banks = [
@@ -56,47 +58,45 @@ def banks = [
     'SIP'    : 'Суд по интеллектуальным правам' // ?
 ]
 
+result = [:] as Map<String, Integer>
 
-if(args.length != 1) {
-    sys.err.println("Usage: ${this.class.simpleName} <sts dir>")
-    sys.exit(255)
-}
+['d:/STS1/', 'd:/STS2/'].each {
 
-result = [:] as Map<String,Integer>
+    fp ->
+        new File( fp ).listFiles().findAll {
+            it.file && it.name ==~ /(?i).*\.STT$/
+        }.each {
+            f ->
+                f.withDataInputStream {
+                    DataInputStream dis ->
+                        def buf = new byte[7]
+                        dis.readFully(buf, 0, buf.length)
 
-new File(args[0]).listFiles().findAll {
-    it.file && it.name ==~ /(?i).*\.STT$/
-}.each {
-    f ->
-        f.withDataInputStream {
-            DataInputStream dis ->
-                def buf = new byte[7]
-                dis.readFully(buf, 0, buf.length)
-
-                if(
-                    buf[0..2] == "STT".bytes as List<Byte> &&
-                    // TODO: buf[3..4] == CRC() ?
-                    buf[5..6] == [0, 0] as List<Byte>
-                ) {
-                    new InflaterInputStream(dis).eachLine {
-                        m = it =~ /(?i)^DOC_OPEN_IM\s+([^\s]+).*/
-                        if(m.matches()) {
-                            def key = m.group(1)
-                            if(  result.containsKey(key)  )
-                                result[key] ++
-                            else
-                                result[key] = 1
+                        if (
+                        buf[0..2] == "STT".bytes as List<Byte>
+                        // TODO: buf[3..4] == CRC() ?
+                        ) {
+                            new InflaterInputStream(dis).eachLine {
+                                m = it =~ /(?i)^DOC_OPEN_IM\s+([^\s]+).*/
+                                if (m.matches()) {
+                                    def key = m.group(1)
+                                    if (result.containsKey(key))
+                                        result[key]++
+                                    else
+                                        result[key] = 1
+                                }
+                                null
+                            }
+                        } else {
+                            throw new IOException("'${f.name}' not have a STT header")
                         }
-                        null
-                    }
-                } else {
-                    throw new IOException("'${f.name}' not have a STT header")
                 }
         }
 }
 
 
-result.sort { a,b -> b.value <=> a.value } each {
-    k,v ->
+
+result.sort { a, b -> b.value <=> a.value } each {
+    k, v ->
         printf("%04d times: '${banks[k]}'\n", v)
 }
